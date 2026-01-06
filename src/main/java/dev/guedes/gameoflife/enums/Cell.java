5f54e36c832633a5573f253379e3044d86ca5793
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2026-01-06 01:09:16 -0300

    :sparkles: feat: add Cell enum to represent cell states

---
+++ src/main/java/dev/guedes/gameoflife/enums/Cell.java
@@ -0,0 +1,18 @@
+package dev.guedes.gameoflife.enums;
+
+/**
+ * Represents the state of a cell in the Game of Life.
+ * <p>
+ * A cell can be either:
+ * <ul>
+ *     <li>{@link #ALIVE} – the cell is currently alive</li>
+ *     <li>{@link #DEAD} – the cell is currently dead</li>
+ * </ul>
+ * </p>
+ *
+ * @author João Guedes
+ */
+public enum Cell {
+    ALIVE,
+    DEAD
+}
