4540594aff6cee3d25b809b302c54bed373400ef
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2026-02-12 02:38:38 -0300

    :sparkles: feat: add base view interfaces and navigation actions

---
+++ src/main/java/dev/guedes/gameoflife/enums/ViewAction.java
@@ -0,0 +1,14 @@
+package dev.guedes.gameoflife.enums;
+
+/**
+ * Defines the possible navigation actions within the application.
+ *
+ * @author João Guedes
+ */
+public enum ViewAction {
+    DISPLAY_MAIN_MENU,
+    DISPLAY_GAME_CONFIG,
+    DISPLAY_GAME_RULES,
+    DISPLAY_GAME_GRID,
+    EXIT_APP
+}
