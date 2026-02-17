2b3138be221cfe000dee89c0db28d4a9bea3efb4
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2026-02-17 00:56:53 -0300

    :sparkles: feat: add base Frame component with default settings

---
+++ src/main/java/dev/guedes/gameoflife/views/gui/components/frame/Frame.java
@@ -0,0 +1,33 @@
+package dev.guedes.gameoflife.views.gui.components.frame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.HeadlessException;
+import java.util.Optional;
+
+/**
+ * A reusable frame class for the application.
+ *
+ * @author João Guedes
+ */
+public class Frame extends JFrame {
+    public Frame(String title, int width, int height) throws HeadlessException {
+        super(title);
+        this.setupDefaultSettings(width, height);
+        this.loadIcon("/assets/icon.png");
+    }
+
+    private void setupDefaultSettings(int width, int height) {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(width, height);
+        setLocationRelativeTo(null);
+    }
+
+    private void loadIcon(String path) {
+        Optional.ofNullable(getClass().getResource(path))
+                .map(ImageIcon::new)
+                .map(ImageIcon::getImage)
+                .ifPresent(this::setIconImage);
+    }
+}
