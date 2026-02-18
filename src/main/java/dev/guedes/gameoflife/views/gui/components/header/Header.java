91214127f0970fcb536ba8fdd36adf6c1135d136
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2026-02-18 11:54:24 -0300

    :art: style: rename Header constants names

--- src/main/java/dev/guedes/gameoflife/views/gui/components/header/Header.java
+++ src/main/java/dev/guedes/gameoflife/views/gui/components/header/Header.java
@@ -6,9 +6,9 @@ import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 
 import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.APP_TITLE;
-import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.COLOR_HEADER_BG;
-import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.COLOR_HEADER_FG;
-import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.FONT_HEADER;
+import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.HEADER_BG_COLOR;
+import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.HEADER_FG_COLOR;
+import static dev.guedes.gameoflife.views.gui.constants.GUIStyle.HEADER_FONT;
 
 /**
  * Header panel for the application.
@@ -23,15 +23,15 @@ public class Header extends JPanel {
     public Header() { setupLayout(); addTitle(); }
 
     private void setupLayout() {
-        this.setBackground(COLOR_HEADER_BG);
+        this.setBackground(HEADER_BG_COLOR);
         this.setLayout(new BorderLayout());
         this.setBorder(new EmptyBorder(10, 20, 10, 20));
     }
 
     private void addTitle() {
         JLabel titleLabel = new JLabel(APP_TITLE);
-        titleLabel.setForeground(COLOR_HEADER_FG);
-        titleLabel.setFont(FONT_HEADER);
+        titleLabel.setForeground(HEADER_FG_COLOR);
+        titleLabel.setFont(HEADER_FONT);
 
         this.add(titleLabel, BorderLayout.WEST);
     }
--- src/main/java/dev/guedes/gameoflife/views/gui/constants/GUIStyle.java
+++ src/main/java/dev/guedes/gameoflife/views/gui/constants/GUIStyle.java
@@ -10,14 +10,13 @@ import java.awt.Font;
  * @author João Guedes
  */
 public final class GUIStyle {
-    private GUIStyle() {}
-
     public static final String APP_TITLE = "Conway’s Game of Life";
     public static final int APP_DEFAULT_WIDTH = 1000;
     public static final int APP_DEFAULT_HEIGHT = 650;
 
-    public static final Color COLOR_HEADER_BG = new Color(51, 102, 187);
-    public static final Color COLOR_HEADER_FG = Color.WHITE;
+    public static final Color HEADER_BG_COLOR = new Color(51, 102, 187);
+    public static final Color HEADER_FG_COLOR = Color.WHITE;
+    public static final Font HEADER_FONT = new Font("SansSerif", Font.PLAIN, 24);
 
-    public static final Font FONT_HEADER = new Font("SansSerif", Font.PLAIN, 24);
+    private GUIStyle() {}
 }
